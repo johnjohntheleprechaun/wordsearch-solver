49f5e509ef4dcd8ad1ecd3250d4cc28bc0eb3249
John Jorgensen <john@jorgensenfamily.us>
2022-09-11 00:01:18 -0600

    create test function

--- grid_parser.py
+++ grid_parser.py
@@ -1,2 +1,16 @@
+import cv2
+import numpy
 def str_to_grid(string: str):
     return [[letter for letter in row] for row in string.split("\n")]
+
+def img_to_grid(img): # grayscale image
+    pass
+
+
+def test():
+    img = cv2.imread("test_data/cropped_word_search.png", cv2.IMREAD_GRAYSCALE)
+    cv2.imshow("test", img)
+    print(img_to_grid(img))
+    cv2.waitKey(0)
+    pass
+test()
