4d59f57e7f2247debb8395fe1634483ad7e54c89
John Jorgensen <john@jorgensenfamily.us>
2022-09-11 01:10:45 -0600

    add grayscale to binary image conversion

--- grid_parser.py
+++ grid_parser.py
@@ -1,16 +1,18 @@
 import cv2
-import numpy
+
 def str_to_grid(string: str):
     return [[letter for letter in row] for row in string.split("\n")]
 
 def img_to_grid(img): # grayscale image
     pass
 
-
 def test():
+    # read image as grayscale
     img = cv2.imread("test_data/cropped_word_search.png", cv2.IMREAD_GRAYSCALE)
-    cv2.imshow("test", img)
-    print(img_to_grid(img))
+    # convert to binary image
+    (thresh, bin_img) = cv2.threshold(img, 128, 256, cv2.THRESH_BINARY)
+    # display binary image
+    cv2.imshow("test", bin_img)
     cv2.waitKey(0)
-    pass
+
 test()
